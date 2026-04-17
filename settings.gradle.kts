adee775fe0e8934a504c59b33e23fc587d78172c
zhangzhonghao <84320236@qq.com>
2026-04-17 15:11:47 +0800

    chore: 配置Gradle镜像源和备用仓库
    - gradle-wrapper.properties 添加阿里云/清华镜像注释
    - settings.gradle.kts 添加华为云/腾讯云备用仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,10 @@ pluginManagement {
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        // 可以添加其他镜像作为备用
+        maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
